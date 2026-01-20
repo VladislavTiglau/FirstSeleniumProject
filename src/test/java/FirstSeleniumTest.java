d55c8c391870b2372a844e81a74b615d8f70f1e8
VladislavTiglau <vladt199882@gmail.com>
2026-01-20 12:56:27 +0100

    Updated

---
+++ src/test/java/FirstSeleniumTest.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class FirstSeleniumTest {
+
+     WebDriver driver;
+
+
+    //before - setUP
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        // driver.get("https://www.google.com/"); //without history
+        driver.navigate().to("https://www.google.com/"); // with history
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
+    }
+
+    //test
+    @Test
+    public void openGoogle() {
+        System.out.println("Google is opened");
+
+    }
+
+    //after - tearDown
+    @AfterMethod
+    public void tearDown() {
+        //driver.quit(); // close browser
+    }
+}
